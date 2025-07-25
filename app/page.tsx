cd2f3d2c22e03880e1422132308553dde102371f
8pxl <56054380+8pxl@users.noreply.github.com>
2025-07-25 13:41:19 -0400

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Starfield from "@/components/starfield";
+import ScrollWrapper from "@/components/scrollWrapper";
+
 export default function Home() {
+  const height = 5000;
   return (
     <main>
-      <div>Hello world!</div>
+      <ScrollWrapper
+        moving = {
+          <div className={"js-bold flex gap-10 p-10 grainy-background h-[5000px]"} >
+                    {/* <title>8198X</title> */}
+                    <div className="w-screen h-25 justify-center text-8xl js-bold text-white text-center mt-25 outline-1">
+                      Lorem ipsum dolor
+                    </div>
+
+            <Starfield height={height}></Starfield>
+          </div>
+        }
+      />
+
     </main>
   );
 }
+
