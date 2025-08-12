e7ce52733ddedf94304d9a763f851dab71cd31ec
8pxl <56054380+8pxl@users.noreply.github.com>
2025-08-12 12:11:30 +0100

    add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+     <div className="grainy-background flex justify-center">
+        {/* <div className="text-white text-5xl font-js bg-white z-1">
+            please wait :)
+        </div> */}
+     </div>   
+    )
+}
