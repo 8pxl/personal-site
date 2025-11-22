35ef95aef02ceb44918efaaffcd484a363bc321a
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-23 00:53:25 +0800

    move head and main tags into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import "@/components/transition.tsx"
+import "@/components/providers"
 import "./globals.css";
-import TransitionOverlay from "@/components/transition";
+import Providers from "@/components/providers";
 import Tabbar from "@/components/tabbar";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "keijay.me",
@@ -16,11 +17,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>keijay huang</title>
+        <meta name="keijay huang" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap');
+        </style>
+        <link rel="preload" as="image" href="/assets/img1.jpg" />
+        <link rel="preload" as="image" href="/assets/img2.jpg" />
+        <link rel="preload" as="image" href="/assets/img3.jpg" />
+      </Head>
       <body>
-        <Tabbar />
-        {/* <TransitionOverlay> */}
-        {children}
-        {/* </TransitionOverlay> */}
+        <main>
+          <Tabbar />
+          <Providers>
+            {children}
+          </Providers>
+        </main>
       </body>
     </html >
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import ScrollWrapper from "@/components/scrollWrapper";
 import Hero from "@/components/hero";
 import SocialLinks from "@/components/socials";
 import Email from "@/components/email";
-import Head from "next/head";
 import Divider from "@/components/divider";
 import About from "@/components/about";
 import Works from "@/components/works";
@@ -14,51 +13,28 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   const height = 1000;
-  // intro();
   return (
-    <>
-      <Head>
-        <title>keijay huang</title>
-        <meta name="keijay huang" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-
-        <style>
-          @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap');
-        </style>
-        <link rel="preload" as="image" href="/assets/img1.jpg" />
-        <link rel="preload" as="image" href="/assets/img2.jpg" />
-        <link rel="preload" as="image" href="/assets/img3.jpg" />
-      </Head>
-      <main>
-        <ScrollWrapper
-          moving={
-            <div className={"p-0 grainy-background"} >
-              <div className="flex flex-col">
-                <Hero />
-                <Divider />
-                <About />
-                <Divider />
-                <Works />
-                <Footer />
-              </div>
-              <Starfield height={height} />
-            </div>
-          }
-          fixed={
-            <div>
-              <SocialLinks />
-              <Email />
-            </div>
-          }
-        />
-      </main>
-    </>
+    <ScrollWrapper
+      moving={
+        <div className={"p-0 grainy-background"} >
+          <div className="flex flex-col">
+            <Hero />
+            <Divider />
+            <About />
+            <Divider />
+            <Works />
+            <Footer />
+          </div>
+          <Starfield height={height} />
+        </div>
+      }
+      fixed={
+        <div>
+          <SocialLinks />
+          <Email />
+        </div>
+      }
+    />
   );
 }
 
