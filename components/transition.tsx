eeee8e08cb0a46742c2590e068e5024a41d007fe
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-24 13:48:13 +0800

    get rid of magic numbers in transisiotn

--- components/transition.tsx
+++ components/transition.tsx
@@ -1,15 +1,33 @@
 // "use client"
 
 export default function TransitionLayer() {
+  const col3 = "#23223D"
+  const col2 = "#3F3D63"
+  const col1 = "#726FA6"
   return (
     <>
-      <div id="transitionCircle" className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-                rounded-full bg-white w-[50px] h-[50px] scale-0 z-50">
-      </div>
+      <div id="transitionCircle1" className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                rounded-full w-[1px] h-[1px] scale-none z-50"
+        style={{
+          background: col1
+        }}>
+      </div >
+
+
+      <div id="transitionCircle2" className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                rounded-full w-[1px] h-[1px] scale-none z-50"
+        style={{
+          background: col2
+        }}>
+      </div >
 
       <div id="transitionRing" className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-                rounded-full outline-[0px] outline-white bg-white w-[1px] h-[1px] scale-none z-51">
-      </div>
+                rounded-full outline-[0px] w-[1px] h-[1px] scale-none z-51"
+        style={{
+          outlineColor: col3,
+          background: col3
+        }}>
+      </div >
     </>
   )
 }
