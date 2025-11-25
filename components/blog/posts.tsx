2236c9259b8a73f3f11492d0f9c4a80be52772f8
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-25 10:09:42 +0800

    move post into mdx

--- components/blog/posts.tsx
+++ components/blog/posts.tsx
@@ -1,49 +1,17 @@
+import { getPosts } from "@/util/postLoader";
 import Card from "./card";
 
 export default function Posts() {
+  const posts = getPosts();
+  console.log(posts)
   return (
     <div className="w-[100vw] flex flex-col items-center justify-center gap-0 text-white font-js">
-      <Card title="the bygone memory of you">
-        the bygone memory of you <br />
-        never bid me adieu<br />
-        it stirs from time to time <br />
-        in the recesses of my mind <br />
-        <br />
-        the bygone you <br />
-        still wears that green cardigan <br />
-        still grows green onions and somehow <br />
-        loves me still <br />
-        <br />
-        everyone tells me that the you i knew <br />
-        the you in my dreams <br />
-        the you by my side <br />
-        is bygone. <br />
-        <br />
-        and maybe i want it to be <br />
-        <br />
-        oh but i can’t forget <br />
-        so how can it really be <br />
-        bygone? how can it be <br />
-        gone, begone, begone! <br />
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-      <Card title="hello">
-        world
-      </Card>
-    </div>
+      {posts.map(post => (
+        < Card title="hello" >
+          {post.slug}
+        </Card>
+      )}
+
+    </div >
   )
 }
